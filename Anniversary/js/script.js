06b0f6381b1f5e96d06759f15674302066cab0f5
HeinLinHtet-24 <52436569+HeinLinHtet-24@users.noreply.github.com>
2025-05-22 00:36:47 +0800

    Delete Anniversary directory

--- Anniversary/js/script.js
+++
@@ -1,9 +0,0 @@
-function calculateDays() {
-    const startDate = new Date(2021, 4, 5); // May is month 4 (0-based)
-    const today = new Date();
-    const diffTime = today - startDate;
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    document.getElementById("daysCounter").textContent = diffDays;
-  }
-  
-  calculateDays();
